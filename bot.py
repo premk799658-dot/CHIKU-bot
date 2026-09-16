64ae5cb164889e5eac283f865771f19d08cb72d9
premk799658-dot <premk799658@gmail.com>
2026-09-16 16:58:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,72 +3,51 @@ from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.environ.get("8918005382:AAFW5TbaLSeGETCS5FU6psiTlazwwZ-j9Hc")
+BOT_TOKEN = os.environ.get("TOKEN")
 OWNER = "PREM BHUMIHAR"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = f"""
-📌 PREFIX-> `/` or `.`
-🎀 OWNER-> {PREM} ✓
-🧿 STATS-> ALIVE
+PREFIX-> / or .
+OWNER-> {OWNER}
+STATS-> ALIVE
 
-⊢≫Nc's
-.fdnc → PREM
-.isaginc → BHUMIHAR
-.dnovnc → KILLER
-.flagnc → 520
-.hrtnc → BHUMIHAR
-.nonc → PREM
-.dicnc → PREM
-.spdnc → BHUMIHAR
-.exabotnc → KILLER
-.mync → 520
-.mxnc → PREM
-.writnc → BHUMIHAR
-.firenc → KILLER
-.favshit → {OWNER}
-.ultranc → PREM BAAP
+Nc's
+.fdnc -> PREM
+.isaginc -> BHUMIHAR
+.dnovnc -> KILLER
+.flagnc -> 520
+.hrtnc -> BHUMIHAR
+.nonc -> PREM
 
-⊢≫SLIDER
-.s | .slider | .special | .anis | .swipe
-
-⊢≫PHOTO
-.savephoto | .PREMph
+SLIDER
+.s | .slider
 """
-    await update.message.reply_text
+    await update.message.reply_text(text)
 
-# (tatte Teri maa ki chut ki awaz kashmir tak pahucha dunga 🫩)
 async def slider(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    OWNER = "PREM BHUMIHAR"  
-    
-    
-    # ("tatte Teri maa ki chut ki awaz kashmir tak pahucha dunga 🫩){PREM} - SABKA BAAP HAI
+    msg = f"Slide by {OWNER} - SABKA BAAP HAI"
     if update.message.reply_to_message:
-        target_name = update.message.reply_to_message.from_user.first_name
-        await update.message.reply_text(f"{PREM BAAP NE TUJHE To Pel Diya\)
+        name = update.message.reply_to_message.from_user.first_name
+        await update.message.reply_text(f"{name} ko pel diya\n{msg}")
     else:
-        await update.message.reply_text(tatte Teri maa ki chut ki awaz kashmir tak pahucha dunga )
+        await update.message.reply_text(msg)
 
 async def all_nc(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"NC DONE BY {PREM} 👑")
+    await update.message.reply_text(f"NC DONE BY {OWNER}")
 
 def run_bot():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler(/start .start)
-    app.add_handler(CommandHandler(/help .start)
-    app.add_handler(CommandHandler("menu", start))
+    app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("slider", slider))
     app.add_handler(CommandHandler("s", slider))
-    app.add_handler(CommandHandler("special", slider))
-    app.add_handler(CommandHandler("anis", slider))
-    app.add_handler(CommandHandler("swipe", slider))
-    for c in ["fdnc","isaginc","dnovnc","flagnc","hrtnc","nonc","dicnc","spdnc","exabotnc","mync","mxnc","writnc","firenc","favshit","ultranc","savephoto","isagiph"]:
+    for c in ["fdnc","isaginc","dnovnc","flagnc","hrtnc","nonc"]:
         app.add_handler(CommandHandler(c, all_nc))
     app.run_polling()
 
 flask_app = Flask(__name__)
 @flask_app.route('/')
-def home(): return f"{prem} BOT RUNNING"
+def home(): return f"{OWNER} BOT RUNNING"
 
 if __name__ == '__main__':
     threading.Thread(target=run_bot).start()
